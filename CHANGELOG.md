76765007352cf6c7bc885cb4a07cc4dd60170250
Simone Melidoni <simone.melidoni@gmail.com>
2023-04-16 13:33:02 +0100

    feature/main-menu (#2)
    * Updated to Node v18.16.0
    
    * Added Main Menu and initial pages setup
    
    - Added Main Menu
    - Added initial Single Player page
    - Added initial 2 Player page

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# WIP
+
+- Upgraded Node version from v18.10.0 to v18.16.0
+- Added Main Menu and initial page setup
+  - Added initial Single Player page
+  - Added initial 2 Player page
---
+++ src/components/global-button/global-button.component.tsx
@@ -0,0 +1,34 @@
+import './global-button.styles.scss';
+
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+interface GlobalButtonProps {
+  label: string;
+  route: string;
+}
+
+const GlobalButton: FC<GlobalButtonProps> = ({ label, route }) => {
+  const retroBackLabel = (
+    <>
+      <span className="global-button-arrow">←</span>
+      <span className="global-button-text">Back</span>
+    </>
+  );
+
+  return (
+    <div className="global-button-container">
+      {route ? (
+        <Link to={route} className="global-button">
+          {label === '<- Back' ? retroBackLabel : label}
+        </Link>
+      ) : (
+        <div className="global-button">
+          {label === '<- Back' ? retroBackLabel : label}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GlobalButton;
