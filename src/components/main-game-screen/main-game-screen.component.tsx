5af7454bebfdf107057399f905ff080ed7cd0db2
Simone Melidoni <simone.melidoni@gmail.com>
2023-04-19 20:33:19 +0100

    feature/main-game-functionality (#4)
    * Added main tic-tac-toe game functionality
    
    * Added comments for future improvements
    
    * Added 'Round' counter after every round/game
    
    * Updated changelog.md

--- src/components/main-game-screen/main-game-screen.component.tsx
+++ src/components/main-game-screen/main-game-screen.component.tsx
@@ -1,33 +1,60 @@
 import './main-game-screen.styles.scss';
 
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback } from 'react';
+
+import TicTacToe from '../tic-tac-toe/tic-tac-toe.component';
+
+/*TO DO:
+  - Make more responsive (mobile and desktop)
+  - Add some sort of animation when selecting square (transform 'x' or 'o')
+  - Add redux/local storage for storing scores
+    - Add styling for scores
+  - Add better animation for when three match
+  - Add Reset Game button
+  - Add Round [i] functionality (to track how many games played)
+  - Comment code
+*/
 
 const MainGameScreen: FC = () => {
+  const [isXNext, setIsXNext] = useState<boolean>(true);
+  const [popupMessage, setPopupMessage] = useState<string | null>(null);
+  const [round, setRound] = useState<number>(1);
+
+  const handlePopup = useCallback((message: string | null) => {
+    setPopupMessage(message);
+  }, []);
+
+  const handleGameOver = useCallback(
+    (winner: 'X' | 'O' | 'draw') => {
+      console.log('Game Over. Winner:', winner);
+      setRound((prevRound) => prevRound + 1);
+      setTimeout(() => {
+        handlePopup(null);
+      }, 3000);
+    },
+    [handlePopup]
+  );
+
   return (
     <div className='main-game-screen-wrapper'>
+      {popupMessage && <div className='popup'>{popupMessage}</div>}
       <div className='main-game-screen-container'>
-        <header className='main-game-screen-header'>Round 1</header>
+        <header className='main-game-screen-header'>Round {round}</header>
         <div className='main-game-screen-content'>
           <div className='player-turn'>
-            <span className='tic-tac-toe-piece-x active'>X</span>
-            <span className='tic-tac-toe-piece-o active'>O</span>
+            <span className={`tic-tac-toe-piece-x${isXNext ? ' active' : ''}`}>
+              X
+            </span>
+            <span className={`tic-tac-toe-piece-o${!isXNext ? ' active' : ''}`}>
+              O
+            </span>
           </div>
-          <div className='tic-tac-toe-board'>
-            {[...Array(2)].map((_, index) => (
-              <div
-                key={`horizontal-${index}`}
-                className='tic-tac-toe-grid-line horizontal'
-                style={{ top: `${(index + 1) * 33.33}%` }}
-              />
-            ))}
-            {[...Array(2)].map((_, index) => (
-              <div
-                key={`vertical-${index}`}
-                className='tic-tac-toe-grid-line vertical'
-                style={{ left: `${(index + 1) * 33.33}%` }}
-              />
-            ))}
-          </div>
+          <TicTacToe
+            onGameOver={handleGameOver}
+            isXNext={isXNext}
+            setIsXNext={setIsXNext}
+            handlePopup={handlePopup}
+          />
         </div>
       </div>
     </div>
